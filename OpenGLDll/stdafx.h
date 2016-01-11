4102b2a8c465422a20325e2c9d15cd7846d648a5
Justin Lycklama <lycklama.justin@gmail.com>
2016-01-10 22:50:21 -0500

    Replace original Project / Solution with a Project setup to create a dll

---
+++ OpenGLDll/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+#include <windows.h>
+#include <GL/glew.h>
+#include <GL/glfw.h>
+#include <glm/glm.hpp>
+
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <cmath>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
